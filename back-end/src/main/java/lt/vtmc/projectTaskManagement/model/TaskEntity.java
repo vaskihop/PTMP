a03260e544bdd5fba09cc895eedfbcd17a2b2f37
silvija <silvija.norkunaite@gmail.com>
2020-04-22 16:04:52 +0300

    added creation and update timestamps to task entity

--- back-end/src/main/java/lt/vtmc/projectTaskManagement/model/TaskEntity.java
+++ back-end/src/main/java/lt/vtmc/projectTaskManagement/model/TaskEntity.java
@@ -1,5 +1,8 @@
 package lt.vtmc.projectTaskManagement.model;
 
+
+import java.util.Date;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,9 +12,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,6 +38,11 @@ public class TaskEntity {
 	@Enumerated(EnumType.STRING)
 	private State taskState;
 	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updatedAt;
+	
 	@ManyToOne (cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JsonIgnore
 	private ProjectEntity project;
@@ -44,7 +55,39 @@ public class TaskEntity {
 		this.taskPriority = taskPriority;
 		this.taskState = taskState;
 		this.project = project;
+		createdAt=new Date();
+		updatedAt=new Date();
 	}
+	
+	public TaskEntity(Task task, ProjectEntity project) {
+		super();
+		this.taskTitle = task.getTaskTitle();
+		this.taskDescription = task.getTaskDescription();
+		this.taskPriority = Priority.valueOf(task.getTaskPriority());
+		this.taskState = State.valueOf(task.getTaskState());
+		
+		createdAt=new Date();
+		updatedAt=new Date();
+
+		this.project = project;
+	}
+
+	public void updateTask(Task task) {
+
+		this.taskTitle = task.getTaskTitle();
+		this.taskDescription = task.getTaskDescription();
+		this.taskPriority = Priority.valueOf(task.getTaskPriority());
+		this.taskState = State.valueOf(task.getTaskState());
+
+		this.updatedAt = new Date();
+
+	}
+
+
+
+	
+	
+	
 	
 	
 	
